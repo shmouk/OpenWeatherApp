dbc24f80790d5cb04309eebb19f2291d152d2fe7
Mark <maretski.mark@gmail.com>
2023-12-08 13:07:10 +0300

    Initial Commit

---
+++ OpenWeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenWeatherApp
+//
+//  Created by Марк on 8.12.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
